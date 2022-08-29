fc38c35312ad4e79412a30ba86d5c3aafae51c3c
SILICON-HAWK <93534675+SILICON-HAWK@users.noreply.github.com>
2022-08-29 11:16:12 +0530

    code commit test1

---
+++ Tester.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import numpy as np
+import math
+
+from cvzone.ClassificationModule import Classifier
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+offset = 20
+imgSize = 300
+
+folder = 'A'
+counter = 0
+
+labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "M"]
+
+while True:
+    success, img = cap.read()
+    imgOut = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y-offset:y+h + offset, x - offset:x+w+offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h/w
+
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal)/2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite)
+            print(prediction, index)
+
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal)/2)
+            imgWhite[hGap:hCal+hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite)
+
+        cv.putText(imgOut, labels[index], (x, y-20), cv.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
+
+        imgCrop_flip = cv.flip(imgWhite, 1)                                  # code to flip image
+        cv.imshow("Image Cropped", imgCrop_flip)
+
+    cv.imshow("Image", imgOut)
+
+    cv.waitKey(1)
---
+++ dataCollection.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+import numpy as np
+import math
+import time
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+
+folder = 'M'
+counter = 0
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y-offset:y+h + offset, x - offset:x+w+offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h/w
+
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal)/2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal)/2)
+            imgWhite[hGap:hCal+hGap, :] = imgResize
+
+        imgCrop_flip = cv.flip(imgWhite, 1)                                  # code to flip image
+        cv.imshow("Image Cropped", imgCrop_flip)
+
+    cv.imshow("Image", img)
+    key = cv.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        # cv.im write(f'{folder}/Image_{time.time()}.jpg', imgCrop_flip)
+        cv.imwrite(f'C:/Users/vikal/PycharmProjects/ASLHandDetection/venv/Data/{folder}/Image_{time.time()}.jpg', imgCrop_flip)
+        print(counter)
+
